55e8fc4d9f4d4e480643a429629dcba7a8bdc758
Valeriy <valera.zlb@outlook.com>
2020-03-13 10:30:40 +0300

    Add few new apps..

--- Troelsen_Csharp_bookCode/Chapter 3/BasicConsoleIO.cs
+++ Troelsen_Csharp_bookCode/Chapter 3/BasicConsoleIO.cs
@@ -7,7 +7,13 @@ namespace BasicConsoleIO
         static void Main(string[] args)
         {
             Console.WriteLine("***** Basic Console I/O *****");
+            
             GetUserData();
+            Console.WriteLine();
+
+            FormatNumericalData();
+            Console.WriteLine();
+            
             Console.ReadLine();
         }
         private static void GetUserData()
@@ -23,6 +29,19 @@ namespace BasicConsoleIO
             Console.WriteLine("Hello {0}! You are {1} years old.", userName, userAge);
 
             Console.ForegroundColor = prevColor;
+        }
+        static void FormatNumericalData()
+        {
+            Console.WriteLine("The value 99999 in various formats:");
+            Console.WriteLine("c format: {0:c}", 99999);
+            Console.WriteLine("d9 format: {0:d9}", 99999);
+            Console.WriteLine("f3 format: {0:f3}", 99999);
+            Console.WriteLine("n format: {0:n}", 99999);
+
+            Console.WriteLine("E format: {0:E}", 99999);
+            Console.WriteLine("e format: {0:e}", 99999);
+            Console.WriteLine("X format: {0:X}", 99999);
+            Console.WriteLine("x format: {0:x}", 99999);
         }
     }
 }
--- Troelsen_Csharp_bookCode/Chapter 3/SimpleCSharpApp.cs
+++ Troelsen_Csharp_bookCode/Chapter 3/SimpleCSharpApp.cs
@@ -9,10 +9,21 @@ namespace SimpleCSharpApp
             Console.WriteLine("***** My First C# App *****");
             Console.WriteLine("Hello World!");
             Console.WriteLine();
-
+            ShowEnvironmentDetails();
             Console.ReadLine();
 
             return -1;
+        }
+        static void ShowEnvironmentDetails()
+        {
+            foreach (string drive in Environment.GetLogicalDrives())
+                Console.WriteLine("Drive: {0}", drive);
+
+            Console.WriteLine("OS: {0}", Environment.OSVersion);
+
+            Console.WriteLine("Number of processofrs: {0}", Environment.ProcessorCount);
+
+            Console.WriteLine(".NET Version: {0}", Environment.Version);
         }
     }
 }
