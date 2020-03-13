348df256e90bee062a356973277a486bb4fb5177
Valeriy <valera.zlb@outlook.com>
2020-03-13 12:46:33 +0300

    more code..

---
+++ Troelsen_Csharp_bookCode/Chapter 3/FunWithStrings.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace FunWithStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicStringFunctionality();
+            StringConcatenation();
+            EscapeChars();
+            DefiningVerbatimStrings();
+            StringEquality();
+
+            Console.ReadLine();
+        }
+        static void BasicStringFunctionality()
+        {
+            Console.WriteLine("=> Basic String functionality:");
+            string firstName = "Freddy";
+            Console.WriteLine("Value of firstName: {0}", firstName);
+            Console.WriteLine("firstName has {0} characters.", firstName.Length);
+            Console.WriteLine("firsftName in uppercase: {0}", firstName.ToUpper());
+            Console.WriteLine("firsftName in lowercase: {0}", firstName.ToLower());
+            Console.WriteLine("firstName contains the letter y?: {0}", firstName.Contains("y"));
+            Console.WriteLine("firstName after replace: {0}", firstName.Replace("dy", ""));
+            Console.WriteLine();
+        }
+        static void StringConcatenation()
+        {
+            Console.WriteLine("=> String concatenation:");
+            string s1 = "Programming the ";
+            string s2 = "PsychoDrill (PTP)";
+            string s3 = s1 + s2;
+            Console.WriteLine(s3);
+            Console.WriteLine();
+        }
+        static void EscapeChars()
+        {
+            Console.WriteLine("=> Escape characters:\a");
+            string strWithTabs = "Model\tColor\tSpeed\tPet Name\a ";
+            Console.WriteLine(strWithTabs);
+
+            Console.WriteLine("Everyone loves \"Hello World\"\a ");
+            Console.WriteLine("C:\\MyApp\\bin\\Debug\a ");
+
+            Console.WriteLine("All finished.\n\n\n\a ");
+            Console.WriteLine();
+        }
+        static void DefiningVerbatimStrings()
+        {
+            Console.WriteLine(@"C:\MyApp\bin\Debug");
+            string myLongString = @"This is a very
+                very
+                    very
+                        long string";
+            Console.WriteLine(myLongString);
+
+            Console.WriteLine(@"Cerebus said ""Darrr! Pret-ty sun-sets""");
+
+            Console.WriteLine();
+        }
+        static void StringEquality()
+        {
+            Console.WriteLine("=> String equality:");
+            string s1 = "Hello!";
+            string s2 = "Yo!";
+            Console.WriteLine("s1 = {0}", s1);
+            Console.WriteLine("s2 = {0}", s2);
+            Console.WriteLine();
+
+            Console.WriteLine("s1 == s2: {0}", s1 == s2);
+            Console.WriteLine("s1 == Hello!: {0}", s1 == "Hello!");
+            Console.WriteLine("s1 == HELLO!: {0}", s1 == "HELLO!");
+            Console.WriteLine("s1 == hello!: {0}", s1 == "hello!");
+            Console.WriteLine("s1.Equals(s2): {0}", s1.Equals(s2));
+            Console.WriteLine("Yo.Equals(s2): {0}", "Yo!".Equals(s2));
+            Console.WriteLine();
+        }
+        static void StringEqualitySpecifyngCompareRules()
+        {
+            Console.WriteLine("=> String equality (Case Insensitive:");
+            string s1 = "Hello!";
+            string s2 = "HELLO!";
+            Console.WriteLine("s1 = {0}", s1);
+            Console.WriteLine("s2 = {0}", s2);
+            Console.WriteLine();
+
+            Console.WriteLine("Default rules: s1 = {0}, s2 = {1}, s1.Equals(s2): {2}",
+                s1, s2, s1.Equals(s2));
+            Console.WriteLine("Ignore case: s1.Equals(s2, StringComparison.OrdinalIgnoreCase): {0}",
+                s1.Equals(s2, StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine("Ignore case, Invariant Culture: s1.Equals(s2, StringComparison.InvariantCultureIgnoreCase): {0}",
+                s1.Equals(s2, StringComparison.InvariantCultureIgnoreCase));
+            Console.WriteLine();
+            Console.WriteLine("Default rules: s1 = {0}, s2 = {1}, s1.IndexOf(\"E\"): {2}",
+                s1, s2, s1.IndexOf("E"));
+            Console.WriteLine("Ignore case, Invarariant Culture: s1.IndexOf(\"E\", " +
+                "StringComparison.InvariantCultureIgnoreCase): {0}",
+                s1.IndexOf("E", StringComparison.InvariantCultureIgnoreCase));
+            Console.WriteLine();
+        }
+    }
+}
