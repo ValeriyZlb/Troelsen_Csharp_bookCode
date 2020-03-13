af8c88eea4e7eeb12dc6f8cae4a9191439c60807
Valeriy <valera.zlb@outlook.com>
2020-03-13 10:05:57 +0300

    Add two new apps

---
+++ Troelsen_Csharp_bookCode/Chapter 3/BasicConsoleIO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BasicConsoleIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Basic Console I/O *****");
+            GetUserData();
+            Console.ReadLine();
+        }
+        private static void GetUserData()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            Console.Write("Please enter your age: ");
+            string userAge = Console.ReadLine();
+
+            ConsoleColor prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Hello {0}! You are {1} years old.", userName, userAge);
+
+            Console.ForegroundColor = prevColor;
+        }
+    }
+}
---
+++ Troelsen_Csharp_bookCode/Chapter 3/SimpleCSharpApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimpleCSharpApp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("***** My First C# App *****");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+
+            Console.ReadLine();
+
+            return -1;
+        }
+    }
+}
