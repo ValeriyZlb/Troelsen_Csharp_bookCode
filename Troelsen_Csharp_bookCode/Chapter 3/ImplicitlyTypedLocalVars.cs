f1f2f473fbaf09ad88f31e5b84354192918a849b
Valeriy <valera.zlb@outlook.com>
2020-03-20 11:31:00 +0300

    add few samples

---
+++ Troelsen_Csharp_bookCode/Chapter 3/ImplicitlyTypedLocalVars.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace ImplicitlyTypedLocalVars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeclareImplicitVars();
+            GetAnInt();
+            LinqQueryOverInt();
+
+            Console.ReadLine();
+        }
+        static void DeclareImplicitVars()
+        {
+            // Неявно типизированные локальные переменные.
+            var myInt = 0;
+            var myBool = true;
+            var myString = "Time, marches on...";
+
+            // Вывести имена лежащих в основе типов.
+            Console.WriteLine("myInt a: {0}", myInt.GetType().Name);    // Вывод типа myInt
+            Console.WriteLine("myBool a: {0}", myBool.GetType().Name);  // Вывод типа myBool
+            Console.WriteLine("myString a: {0}", myString.GetType().Name);  // Вывод типа myString
+        }
+        static int GetAnInt()
+        {
+            var retVal = 9;
+            return retVal;
+        }
+        static void LinqQueryOverInt()
+        {
+            int[] numbers = { 10, 20, 30, 40, 1, 2, 3, 8 };
+
+            // Запрос LINQ!
+            var subset = from i in numbers where i < 10 select i;
+            Console.Write("Values in subset: ");
+            foreach (var i in subset)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+
+            // К какому же типу относится subnet?
+            Console.WriteLine("subnet is a: {0}", subset.GetType().Name);
+            Console.WriteLine("subset is defined in: {0}", subset.GetType().Namespace);
+        }
+    }
+}
---
+++ Troelsen_Csharp_bookCode/Chapter 3/TypeConversions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TypeConversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with type conversions *****");
+            
+            // Добавить две переменные типа short и вывести результат.
+            short numb1 = 30000, numb2 = 30000;
+            
+            // Явно привести int к short (и разрешить потерю данных).
+            short answer = (short)Add(numb1, numb2);
+            Console.WriteLine("{0} + {1} = {2}", numb1, numb2, answer);
+            NarrowingAttempt();
+            ProcessBytes();
+
+            Console.ReadLine();
+        }
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        static void NarrowingAttempt()
+        {
+            byte myByte = 0;
+            int myInt = 200;
+
+            // Явно привести int к byte (без потери данных).
+            myByte = (byte)myInt;
+            Console.WriteLine("Value of myByte: {0}", myByte);
+        }
+        static void ProcessBytes()
+        {
+            byte b1 = 100;
+            byte b2 = 250;
+
+            // На этот раз сообщить компилятору о необходимости добавления
+            // кода CIL, неодходимого для генерации исключения, если возникает
+            // переполнение или потеря значимости.
+            try
+            {
+                checked
+                {
+                    byte sum = (byte)Add(b1, b2);
+                    Console.WriteLine("sum = {0}", sum);
+                }
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
